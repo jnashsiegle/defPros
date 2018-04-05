78860b2deac1bb0c3ffca9ca65f9f6b5740c660a
Jana Siegle <jsiegle@lightpointdev.com>
2018-04-05 15:42:55 -0500

    first commit
    Signed-off-by: Jana Siegle <jsiegle@lightpointdev.com>

---
+++ assets/js/app.js
@@ -0,0 +1,59 @@
+/*$(document).ready(function() {
+  $('#chris.tabs ul li.item, #mike.tabs ul li.item').on('click', function() {
+    var number = $(this).data('option');
+    $('#chris.tabs ul li.item, #mike.tabs ul li.item').removeClass('is-active');
+    $(this).addClass('is-active');
+    $('#tab_container .container_item').removeClass('is-active');
+    $('div[data-item="' + number + '"]').addClass('is-active');
+  });
+});
+
+$(document).ready(function() {
+    $('#tab_container').hide();
+    $('#main').click(function(){
+        $('#bio_info, #certifications_info').hide();
+        $('#main').addClass('is-active');
+        $('#bio, #certifications').removeClass('is-active');
+        $('#main_info').addClass('is-active').show();
+        $('#tab_container').show();
+     });
+     $('#bio').click(function(){
+        $('#main_info, #certifications_info').hide();
+        $('#bio').addClass('is-active');
+        $('#main, #certifications').removeClass('is-active');
+        $('#bio_info').addClass('is-active').show();
+        $('#tab_container').show();
+     });
+     $('#certifications').click(function(){
+     $('#main_info, #bio_info').hide();
+     $('#certifications').addClass('is-active');
+     $('#main, #bio').removeClass('is-active');
+     $('#certifications_info').addClass('is-active').show();
+     $('#tab_container').show();
+  });
+     /*$(document).ready(function() {
+    $('#tab_container').hide();
+    $('#main1').click(function(){
+        $('#bio_info1, #certifications_info1').hide();
+        $('#main1').addClass('is-active');
+        $('#bio1, #certifications1').removeClass('is-active');
+        $('#main_info1').addClass('is-active').show();
+        $('#tab_container').show();
+     });
+     $('#bio1').click(function(){
+        $('#main_info1, #certifications_info1').hide();
+        $('#bio1').addClass('is-active');
+        $('#main1, #certifications1').removeClass('is-active');
+        $('#bio_info1').addClass('is-active').show();
+        $('#tab_container').show();
+     });
+     $('#certifications1').click(function(){
+     $('#main_info1, #bio_info1').hide();
+     $('#certifications1').addClass('is-active');
+     $('#main1, #bio1').removeClass('is-active');
+     $('#certifications_info1').addClass('is-active').show();
+     $('#tab_container').show();
+  });*/
+/*});
+
+*/
